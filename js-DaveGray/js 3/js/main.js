bd1aad16765e0f7c8ed2cbf53669f0cf20433b87
IanWachcode <ianwachira90@gmail.com>
2025-12-23 08:02:07 +0300

    2nd commit

--- js-DaveGray/js 3/js/main.js
+++ js-DaveGray/js 3/js/main.js
@@ -1,26 +1,3 @@
-//console.log('hello world');
-
-//console.log(typeof 'ian'); // string
-//console.log(typeof 3); // number
-//console.log(typeof true); // boolean
-//console.log(typeof {}); // object
-//console.log(typeof []); // object
-//console.log(typeof null); // object
-//console.log(typeof undefined); // undefined
-
-//string methods
-const myVariable = 'Mathematics';
-
-console.log(myVariable.length); // 11
-console.log(myVariable.charAt(5)); // m
-console.log(myVariable.indexOf('t')); // 3
-console.log(myVariable.lastIndexOf('at')); // 1
-console.log(myVariable.slice(5, 8)); // mat
-console.log(myVariable.toUpperCase()); // MATHEMATICS
-console.log(myVariable.toLowerCase()); // mathematics
-console.log(myVariable.includes('math')); // false
-// cspell:ignore matics
-console.log(myVariable.split('e')); // [ 'Math', 'matics' ]
 
 
 
