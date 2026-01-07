0147c0f9b24c4543cb0edd176da58419ab18f66c
IanWachcode <ianwachira90@gmail.com>
2026-01-07 11:10:07 +0300

    3rd commit

--- js-Telusko/script.js
+++ js-Telusko/script.js
@@ -1,2 +1,15 @@
 //document.getElementById('h1').textContent = 'J.S TEST HEADING';
-document.getElementById('ts').textContent = 'Hello, World!';
+//document.getElementById('ts').textContent = 'Hello, World!';
+
+//let num = 2+2;
+//console.log(num);
+
+//let user = 'Wachira';
+//console.log(num + user);
+
+//let num1 = 0xf; //hexadecimal
+//console.log(num1 * 2);
+
+//let num1 = 0o12; //octal
+//console.log(num1);
+
