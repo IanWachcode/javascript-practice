dd91dfccc6d75816af3794f0b39726965acdd2d5
IanWachcode <ianwachira90@gmail.com>
2026-01-13 19:45:14 +0300

    day 4 functions commit

---
+++ js prac classes/21days js/week1/day4/script.js
@@ -0,0 +1,49 @@
+//let lap1 = 40
+//let lap2 = 60
+//let lap3 = 80
+
+//function totalLapTime() {
+   // let total = lap1 + lap2 + lap3
+    //console.log(total)
+    ///console.log(lap1 + lap2 + lap3)
+//}
+//totalLapTime()
+
+//let lapsCompeleted = 0
+
+//function incrementLap() {
+  //  lapsCompeleted = lapsCompeleted + 1
+//}
+
+//incrementLap()
+//incrementLap()
+//incrementLap()
+
+//console.log(lapsCompeleted)
+
+
+//console.log("Hello");
+
+function greet(name) {
+    console.log("Hello" + name);
+
+}
+greet("Wachira");
+greet("John");
+
+function add(a, b) {
+    return a + b;
+}
+ let result = add(5, 3);
+ console.log(result);
+
+ //function multiply(a, b) {
+    //return a * b;
+ //}
+//console.log(multiply(5, 6));
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+console.log(multiply(4, 7));
